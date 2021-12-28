7833da22de9543ad98bdf159539878f9625b82d8
Alison Mukoma <alison.mukoma@trivago.com>
2021-12-28 22:54:43 +0100

    Update auth

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', include('base.urls')),
     path('', include('ppcr.urls', namespace='ppcr')),
     path('', include('tralard.urls', namespace='tralard')),
-    path('', include("authentication.urls")), # Auth routes - login / register
+    path('accounts/', include("authentication.urls")), # Auth routes - login / register
     path('', include(dj_beneficiary_urls)),
 ]
 
