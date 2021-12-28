ddd2593e9e26ee5c6336a5cbf0ececce45639219
Alison Mukoma <alison.mukoma@trivago.com>
2021-12-28 22:19:37 +0100

    add base business workflow layout

--- django_project/authentication/admin.py
+++ django_project/authentication/admin.py
@@ -1,6 +1,4 @@
 # -*- encoding: utf-8 -*-
-
-
 from django.contrib import admin
 
 # Register your models here.
--- django_project/authentication/urls.py
+++ django_project/authentication/urls.py
@@ -1,6 +1,4 @@
 # -*- encoding: utf-8 -*-
-
-
 from django.urls import path
 from .views import login_view, register_user
 from django.contrib.auth.views import LogoutView
