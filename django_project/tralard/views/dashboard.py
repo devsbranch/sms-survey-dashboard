41cdee75b1237fc40a2a4b3c23157b6362aef197
Alison Mukoma <mukomalison@gmail.com>
2022-01-03 19:57:53 +0100

    Clean dash (#31)
    * add project indicator listing
    
    * update dash
    
    * add stats counters

--- django_project/tralard/views/dashboard.py
+++ django_project/tralard/views/dashboard.py
@@ -1,7 +1,11 @@
+from django.db.models import Sum
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from tralard.models.program import Program
+from tralard.models.fund import Fund
+from tralard.models.sub_project import SubProject
+from tralard.models.beneficiary import Beneficiary
 
 
 class HomeTemplateView(LoginRequiredMixin, TemplateView):
@@ -9,8 +13,15 @@ class HomeTemplateView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self):
         context = super(HomeTemplateView, self).get_context_data()
+        self.total_project_funds = Fund.objects.all().aggregate(Sum('amount'))
+        self.cleaned_total_project_funds = self.total_project_funds['amount__sum']
+
         context['title'] = 'Program: Tralard'
         context['program_list'] = Program.objects.all().order_by('-started')[:5]
+        context['program_count'] = Program.objects.all().count()
+        context['total_project_funds'] = self.cleaned_total_project_funds
+        context['subproject_count'] = SubProject.objects.all().count()
+        context['beneficiary_count'] = Beneficiary.objects.all().count()
         return context
 
 class ProjectListView(LoginRequiredMixin, TemplateView):
