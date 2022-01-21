ca4c89d9be316f09c138e2a225eddf515af74b3e
Alison Mukoma <mukomalison@gmail.com>
2022-01-21 14:07:01 +0100

    stabilize map and realign polygons (#123)
    * have radius province locator
    
    * tag requirements
    
    * remove unused file
    
    * verbose name

--- django_project/tralard/map_utils.py
+++ django_project/tralard/map_utils.py
@@ -1,9 +1,12 @@
 import random
-
 import folium
+import pandas as pd
+import altair as alt
 from folium import plugins
 
+from tralard.models.ward import Ward
 from tralard.models.district import District
+from tralard.models.beneficiary import Beneficiary
 from tralard.constants import MAP_LAYER_CHOICES
 
 def prepare_marker_data():
@@ -16,15 +19,30 @@ def prepare_marker_data():
 
     data = {}
     # work in progress
-    for geo_data in district_details:
-        data[f"{geo_data.name}"] = [
-            geo_data.location.x,
-            geo_data.location.y,
-            random.choice(["business-firm", "cooperative"]),
+    for district_data in district_details:
+        data[district_data] = [
+            district_data.location.x,
+            district_data.location.y
+            # random.choice(["business-firm", "cooperative"]),
         ]
 
     return data
 
+def circle_marker(district_name, lat, lng, source):
+    chart = alt.Chart(source).mark_bar().encode(x='Wards', y='Beneficiary Count')
+    visual_graph = chart.to_json()
+    marker = folium.CircleMarker(
+        location=[lat, lng],
+        radius=10,
+        color='darkgreen',
+        fill=True,
+        fill_color='lightblue',
+        fillOpacity=1.0,
+        opacity=0.5,
+        tooltip=district_name,
+        popup=folium.Popup(max_width=500).add_child(folium.VegaLite(visual_graph, width=500, height=300)),
+    )
+    return marker
 
 def build_map_context():
 
@@ -33,8 +51,11 @@ def build_map_context():
         # bounding box for map of zambia
         location=[-13.1519165, 27.852537499999983],
         tiles="cartodbpositron",
-        zoom_start=6.5,
-        # max_bounds=True
+        control_scale=True,
+        max_zoom=8,
+        zoom_start=6.4,
+        width='100%',
+        height='80%',
     )
 
     map_layers = MAP_LAYER_CHOICES
@@ -42,61 +63,54 @@ def build_map_context():
         folium.raster_layers.TileLayer(map_layer).add_to(map)
 
     mini_map = plugins.MiniMap(toggle_display=True)
-
-    # clustering
     marker_cluster = plugins.MarkerCluster().add_to(map)
 
     # Search widget
     search = plugins.Search(
         marker_cluster,
+        geom_type="Point",
         search_label=None,
         search_zoom=None,
-        geom_type="Point",
         position="topleft",
         placeholder="Search district",
         collapsed=False,
     )
-
     search.add_to(map)
 
     # Draw tools
-    draw = plugins.Draw(export=False)
-
-    draw.add_to(map)
-
-    # search.add_to(map)
-
+    draw = plugins.Draw(export=True)
     locate = plugins.LocateControl(auto_start=False)
+    draw.add_to(map)
     locate.add_to(map)
-
-    # add minimap to main map
     map.add_child(mini_map)
-
-    # add scroll zoom toggler to main map
     plugins.Fullscreen(position="topright").add_to(map)
 
     data = prepare_marker_data()
 
-    for district, lat_lng in data.items():
-        if lat_lng[2] == "coorperative" and lat_lng[2] is not None:
-            color = "darkred"
+    ward_names = []
+    beneficiary_count = []
+    for ward in Ward.objects.all():
+        beneficiary_counter = Beneficiary.objects.filter(
+            ward__name=ward.name
+            ).count()
+        beneficiary_count.append(beneficiary_counter)
+        ward_names.append(ward.name.capitalize())
 
-        elif lat_lng[2] == "business-firm" and lat_lng[2] is not None:
-            color = "darkgreen"
+    source = pd.DataFrame(
+            {
+            "Wards": ward_names,
+            "Beneficiary Count": beneficiary_count
+        }
+    )
 
-        else:
-            color = "lightblue"
+    start_coords = []
+    for district_data, lat_lng in data.items():
+        start_coords.append((lat_lng[0], lat_lng[1]))
+        district_name_verbose = f"{district_data.name.capitalize()} District - {district_data.province.name}"
+        marker = circle_marker(district_name_verbose, lat_lng[0], lat_lng[1], source)
+        marker.add_to(marker_cluster)
 
-        folium.Marker(
-            location=[lat_lng[0], lat_lng[1]],
-            popup=district.capitalize(),
-            icon=folium.Icon(color=color, icon="fa-users", prefix="fa"),
-        ).add_to(marker_cluster)
-
-    # # add map layer toggle, intentionally to be initiated after rendering map widget
     folium.LayerControl().add_to(map)
-
-    # render map onto html
     map = map._repr_html_()
 
     return map
